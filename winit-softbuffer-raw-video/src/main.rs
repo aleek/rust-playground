feb7154a3971e9870cfba7e62b585746db395729
Aleksander <adutkowski@gmail.com>
2025-08-30 15:02:53 +0200

    winit softbuffer raw video

---
+++ winit-softbuffer-raw-video/src/main.rs
@@ -0,0 +1,86 @@
+use std::num::NonZeroU32;
+use winit::event::{Event, KeyEvent, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::keyboard::{Key, NamedKey};
+
+#[path = "utils/winit_app.rs"]
+mod winit_app;
+
+#[cfg(not(target_os = "android"))]
+fn main() {
+    entry(EventLoop::new().unwrap())
+}
+
+pub(crate) fn entry(event_loop: EventLoop<()>) {
+    let context = softbuffer::Context::new(event_loop.owned_display_handle()).unwrap();
+
+    let app = winit_app::WinitAppBuilder::with_init(
+        |elwt| winit_app::make_window(elwt, |w| w),
+        move |_elwt, window| softbuffer::Surface::new(&context, window.clone()).unwrap(),
+    )
+    .with_event_handler(|window, surface, event, elwt| {
+        elwt.set_control_flow(ControlFlow::Wait);
+
+        match event {
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::Resized(size),
+            } if window_id == window.id() => {
+                let Some(surface) = surface else {
+                    eprintln!("Resized fired before Resumed or after Suspended");
+                    return;
+                };
+
+                if let (Some(width), Some(height)) =
+                    (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
+                {
+                    surface.resize(width, height).unwrap();
+                }
+            }
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::RedrawRequested,
+            } if window_id == window.id() => {
+                let Some(surface) = surface else {
+                    eprintln!("RedrawRequested fired before Resumed or after Suspended");
+                    return;
+                };
+                let size = window.inner_size();
+                if let (Some(width), Some(height)) =
+                    (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
+                {
+                    let mut buffer = surface.buffer_mut().unwrap();
+                    for y in 0..height.get() {
+                        for x in 0..width.get() {
+                            let red = x % 255;
+                            let green = y % 255;
+                            let blue = (x * y) % 255;
+                            let index = y as usize * width.get() as usize + x as usize;
+                            buffer[index] = blue | (green << 8) | (red << 16);
+                        }
+                    }
+
+                    buffer.present().unwrap();
+                }
+            }
+            Event::WindowEvent {
+                event:
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                logical_key: Key::Named(NamedKey::Escape),
+                                ..
+                            },
+                        ..
+                    },
+                window_id,
+            } if window_id == window.id() => {
+                elwt.exit();
+            }
+            _ => {}
+        }
+    });
+
+    winit_app::run_app(event_loop, app);
+}
